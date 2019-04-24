a2aed553fd8bf427f31de1f6ff3e3174cf4e2c83
Tech-a28 <tech-a28@Tech-A28s-MacBook-Air.local>
2019-04-24 09:24:17 +0100

    added two player controls

--- js/app.js
+++ js/app.js
@@ -2,8 +2,8 @@
 // 1 = <div class="empty"></div>
 // 2 = <div class="breakable"></div>
 // 3 = <div class="non-breakable"></div>
-// 4 = <div class="player"></div>
-// 5 = <div class="goal"></div>
+// 4 = <div class="player1"></div>
+// 5 = <div class="player2"></div>
 // 6 = <div class="bomb"></div>
 
 // array symbolises the game board
@@ -14,21 +14,21 @@ var map = [[4,1,0,0,0,0,0,0,0],
            [0,0,0,0,0,0,0,0,0],
            [0,3,0,3,0,3,0,3,0],
            [0,0,0,0,0,0,0,0,0],
-           [0,3,0,3,0,3,0,3,0],
-           [0,0,0,0,0,0,0,0,5]];
+           [0,3,0,3,0,3,0,3,1],
+           [0,0,0,0,0,0,0,1,5]];
 
-var player = {x: 0, y: 0};
-var lives = 2;
+var player1 = {name: "Player1", indicator: 4, lives: 2, x: 0, y: 0};
+var player2 = {name: "Player2", indicator: 5, lives: 2, x: 8, y: 8};
 
 $(function()
 {
-  $("#lives").html(`Lives: ${lives}`);
+  $("#p1lives").html(`Player 1 Lives: ${player1.lives}`);
+  $("#p2lives").html(`Player 2 Lives: ${player2.lives}`);
 
   drawWorld();
-  if (lives > 0)
-  {
-    playerControl();
-  }
+
+  player1Control();
+  player2Control();
 })
 
 // draws the world with all the required tiles based on the map array
@@ -81,12 +81,12 @@ function drawWorld()
       }
       else if (map[i][j] === 4)
       {
-        $("#world").append(`<div class="player" id="a${i}${j}"></div>`);
+        $("#world").append(`<div class="player1" id="a${i}${j}"></div>`);
         absolutePositions(i,j);
       }
       else if (map[i][j] === 5)
       {
-        $("#world").append(`<div class="goal" id="a${i}${j}"></div>`);
+        $("#world").append(`<div class="player2" id="a${i}${j}"></div>`);
         absolutePositions(i,j);
       }
       else if (map[i][j] === 6)
@@ -98,91 +98,91 @@ function drawWorld()
   }
 }
 
-// function to control player
-function playerControl()
+// function to control player1 with arrow keys
+function player1Control()
 {
   document.onkeydown = function(e)
   {
-    if (lives >= 0)
+    if (player1.lives >= 0)
     {
       switch (e.keyCode)
       {
         // left
         case 37:
         // if the space you want to move to is free
-        if (map[player.y][player.x - 1] === 1)
+        if (map[player1.y][player1.x - 1] === 1)
         {
           // if a bomb has been dropped then tile will remain a bomb
-          if (map[player.y][player.x] === 6)
+          if (map[player1.y][player1.x] === 6)
           {
-            player.x--;
-            map[player.y][player.x] = 4;
+            player1.x--;
+            map[player1.y][player1.x] = 4;
           }
           // if no bomb placed tile you move from will be empty
           else
           {
-            map[player.y][player.x] = 1;
-            player.x--;
-            map[player.y][player.x] = 4;
+            map[player1.y][player1.x] = 1;
+            player1.x--;
+            map[player1.y][player1.x] = 4;
           }
         }
         break;
         // up
         case 38:
-        if(map[player.y - 1][player.x] === 1)
+        if(map[player1.y - 1][player1.x] === 1)
         {
-          if (map[player.y][player.x] === 6)
+          if (map[player1.y][player1.x] === 6)
           {
-            player.y--;
-            map[player.y][player.x] = 4;
+            player1.y--;
+            map[player1.y][player1.x] = 4;
           }
           else
           {
-            map[player.y][player.x] = 1;
-            player.y--;
-            map[player.y][player.x] = 4;
+            map[player1.y][player1.x] = 1;
+            player1.y--;
+            map[player1.y][player1.x] = 4;
           }
         }
         break;
         // right
         case 39:
-        if (map[player.y][player.x + 1] === 1)
+        if (map[player1.y][player1.x + 1] === 1)
         {
-          if (map[player.y][player.x] === 6)
+          if (map[player1.y][player1.x] === 6)
           {
-            player.x++;
-            map[player.y][player.x] = 4;
+            player1.x++;
+            map[player1.y][player1.x] = 4;
           }
           else
           {
-            map[player.y][player.x] = 1;
-            player.x++;
-            map[player.y][player.x] = 4;
+            map[player1.y][player1.x] = 1;
+            player1.x++;
+            map[player1.y][player1.x] = 4;
           }
         }
         break;
         // down
         case 40:
-        if (map[player.y + 1][player.x] === 1)
+        if (map[player1.y + 1][player1.x] === 1)
         {
-          if (map[player.y][player.x] === 6)
+          if (map[player1.y][player1.x] === 6)
           {
-            player.y++;
-            map[player.y][player.x] = 4;
+            player1.y++;
+            map[player1.y][player1.x] = 4;
           }
           else
           {
-            map[player.y][player.x] = 1;
-            player.y++;
-            map[player.y][player.x] = 4;
+            map[player1.y][player1.x] = 1;
+            player1.y++;
+            map[player1.y][player1.x] = 4;
           }
         }
         break;
         case 32:
-        if (map[player.y][player.x] !== 6)
+        if (map[player1.y][player1.x] !== 6)
         {
-          map[player.y][player.x] = 6;
-          Bomb(player.y, player.x);
+          map[player1.y][player1.x] = 6;
+          Bomb(player1, player1.y, player1.x);
         }
         break;
         default:
@@ -190,17 +190,111 @@ function playerControl()
       }
       drawWorld();
     }
+  }
+}
 
+// function to control player2 with
+function player2Control()
+{
+  document.onkeyup = function(e)
+  {
+    if (player2.lives >= 0)
+    {
+      switch (e.keyCode)
+      {
+        // left
+        case 65:
+        // if the space you want to move to is free
+        if (map[player2.y][player2.x - 1] === 1)
+        {
+          // if a bomb has been dropped then tile will remain a bomb
+          if (map[player2.y][player2.x] === 6)
+          {
+            player2.x--;
+            map[player2.y][player2.x] = 5;
+          }
+          // if no bomb placed tile you move from will be empty
+          else
+          {
+            map[player2.y][player2.x] = 1;
+            player2.x--;
+            map[player2.y][player2.x] = 5;
+          }
+        }
+        break;
+        // up
+        case 87:
+        if(map[player2.y - 1][player2.x] === 1)
+        {
+          if (map[player2.y][player2.x] === 6)
+          {
+            player2.y--;
+            map[player2.y][player2.x] = 5;
+          }
+          else
+          {
+            map[player2.y][player2.x] = 1;
+            player2.y--;
+            map[player2.y][player2.x] = 5;
+          }
+        }
+        break;
+        // right
+        case 68:
+        if (map[player2.y][player2.x + 1] === 1)
+        {
+          if (map[player2.y][player2.x] === 6)
+          {
+            player2.x++;
+            map[player2.y][player2.x] = 5;
+          }
+          else
+          {
+            map[player2.y][player2.x] = 1;
+            player2.x++;
+            map[player2.y][player2.x] = 5;
+          }
+        }
+        break;
+        // down
+        case 83:
+        if (map[player2.y + 1][player2.x] === 1)
+        {
+          if (map[player2.y][player2.x] === 6)
+          {
+            player2.y++;
+            map[player2.y][player2.x] = 5;
+          }
+          else
+          {
+            map[player2.y][player2.x] = 1;
+            player2.y++;
+            map[player2.y][player2.x] = 5;
+          }
+        }
+        break;
+        case 82:
+        if (map[player2.y][player2.x] !== 6)
+        {
+          map[player2.y][player2.x] = 6;
+          Bomb(player2,player2.y, player2.x);
+        }
+        break;
+        default:
+        drawWorld();
+      }
+      drawWorld();
+    }
   }
 }
 
 // function to spawn bomb on button press
-// bomb delay/blast
-function Bomb(bombPosY, bombPosX)
+// bomb delay/blast effect
+function Bomb(player, bombPosY, bombPosX)
 {
   setTimeout(function()
   {
-    // destroys blocks/player to the right
+    // destroys blocks/player1 to the right
     if (bombPosY != 0)
     {
       // explodes space above
@@ -238,34 +332,65 @@ function Bomb(bombPosY, bombPosX)
 
     map[bombPosY][bombPosX] = 1;
     drawWorld();
+    LivesCount(player)
 
-    if (playerDead() && lives > 0)
+  }, 1000);
+}
+
+// decrements lives and alters score display
+function LivesCount(player)
+{
+  console.log("test");
+  if (player.name === "Player1" && playerDead(player))
+  {
+    if (player1.lives > 0)
     {
       map[0][0] = 4;
       player.x = 0;
       player.y = 0;
-      lives--;
-      $("#lives").html(`Lives: ${lives}`);
+      player1.lives--;
+      $("#p1lives").html(`Player 1 Lives: ${player1.lives}`);
       drawWorld();
+      console.log(map);
     }
-    else if (playerDead() && lives === 0)
+    else if (player1.lives === 0)
     {
-      $("#lives").html("GAME OVER!!");
-      lives--;
+      $("#p1lives").html("GAME OVER!!");
+      $("#p2lives").html("WINNER!!");
+      player1.lives--;
     }
-  }, 1000);
+  }
+  else if (player.name === "Player2" && playerDead(player))
+  {
+    if (player2.lives > 0)
+    {
+      map[8][8] = 5;
+      player.x = 8;
+      player.y = 8;
+      player2.lives--;
+      $("#p2lives").html(`Player 2 Lives: ${player2.lives}`);
+      drawWorld();
+      console.log(map);
+    }
+    else if (player2.lives === 0)
+    {
+      $("#p2lives").html("GAME OVER!!");
+      $("#p1lives").html("WINNER");
+      player2.lives--;
+    }
+  }
 }
 
-// checks if player is dead
-function playerDead()
+// checks if player1 is dead
+function playerDead(player)
 {
-  var dead = true;
+  var dead = false;
 
   for (var i = 0; i < map.length; i++)
   {
-    if (map[i].includes(4))
+    if (map[i].includes(player.indicator))
     {
-      dead = false;
+      dead = true;
     }
   }
 
